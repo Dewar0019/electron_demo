806deac244e4a8248cb5b5e6b1e1d52cb09bf912
Dewar Tan <dewar@vermonster.com>
2018-01-09 22:11:54 -0500

    Add new item to shopping cart

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require('path');
 const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
-
+let addWindow;
 
 // Listen for app to be ready
 app.on('ready', function() {
@@ -20,6 +20,11 @@ app.on('ready', function() {
         slashes: 'true'
     }));
 
+    // Quit app when closed
+    mainWindow.on('closed', function() {
+       app.quit();
+    });
+
     // Build menu from template
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
 
@@ -27,13 +32,38 @@ app.on('ready', function() {
     Menu.setApplicationMenu(mainMenu);
 });
 
+// Handle create add window
+ function createAddWindow() {
+     // Create new window
+     addWindow = new BrowserWindow({
+         width: 300,
+         height: 200,
+         title: 'Add Shopping List Item'
+     });
+     // Load html into window
+
+     // Passes in file://dirname/main.html
+     addWindow.loadURL(url.format({
+         pathname: path.join(__dirname, 'addWindow.html'),
+         protocol: 'file:',
+         slashes: 'true'
+     }));
+     // Garbage collection handle
+     addWindow.on('close', function() {
+        addWindow = null;
+     })
+ }
+
 // Create menu template
 const mainMenuTemplate = [
     {
         label: 'File',
         submenu: [
             {
-                label: 'Add Item'
+                label: 'Add Item',
+                click() {
+                    createAddWindow();
+                }
             },
             {
                 label: 'Clear Items'
